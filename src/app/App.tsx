85222bc5b0265d11f616705bc865627102308230
Alexander Lvov <iJullic@yandex.ru>
2024-03-26 17:09:26 +0300

    add pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,24 @@
+import 'react-toastify/dist/ReactToastify.css';
+
+import { ToastContainer } from 'react-toastify';
+
+import { Router } from '@/pages';
+
 export const App = () => {
-    return <></>;
+    return (
+        <>
+            <ToastContainer
+                containerId={'top-right'}
+                position='top-right'
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                theme='light'
+            />
+            <Router />
+        </>
+    );
 };
