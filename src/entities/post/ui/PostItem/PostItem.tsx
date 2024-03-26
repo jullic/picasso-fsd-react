b34f3f3d621ad5aa30f1abdcf59ef542996c5e56
Alexander Lvov <iJullic@yandex.ru>
2024-03-26 20:31:55 +0300

    add divider

--- src/entities/post/ui/PostItem/PostItem.tsx
+++ src/entities/post/ui/PostItem/PostItem.tsx
@@ -6,7 +6,7 @@ import { IPostItem } from './PostItem.props';
 
 export const PostItem: FC<IPostItem> = ({ body, id, title, userId, loading, ViewButton, ...props }) => {
     return (
-        <div className='flex gap-4 items-center' {...props}>
+        <div className='flex gap-4 items-center border-b border-solid' {...props}>
             <div className={clsx(styles.root, 'w-full p-4')}>{loading ? 'Loading...' : `${id} | ${title} | ${body}`}</div>
             <ViewButton id={id} />
         </div>
