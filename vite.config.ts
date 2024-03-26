d95e4317eb71e2b7405e66727dd2f979016ddce6
Alexander Lvov <iJullic@yandex.ru>
2024-03-26 15:55:55 +0300

    build: update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import path, { join } from 'path';
+
+const envs = dotenv.config({ path: join(__dirname, '.env') });
+const domain = envs.parsed?.API_DOMAIN || 'jsonplaceholder.typicode.com';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		host: true,
+		port: 3000,
+		proxy: {
+			'/api': {
+				target: `https://${domain}`,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			},
+		},
+	},
+	resolve: {
+		alias: [{ find: '@', replacement: path.resolve('src') }],
+	},
+	css: {
+		modules: {
+			generateScopedName: '[name]__[local]__[hash:8]',
+		},
+	},
+});
